4f049052d113db9fd0c0bbdac2bff37bcbd2849e
userhsh <kdkd477@naver.com>
2024-10-28 15:21:30 +0900

    Feat : Start Scene
    Start Scene

---
+++ Assets/02. Scripts/StartScene/StartManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartManager : MonoBehaviour
+{ 
+    MainPanel mainPanel = null;
+    OptionPanel optionPanel = null;
+
+    bool isOption = false;
+
+    private void Awake()
+    {
+        StartManagerInit();
+    }
+
+    private void StartManagerInit()
+    {
+        mainPanel = GetComponentInChildren<MainPanel>();
+        optionPanel = GetComponentInChildren<OptionPanel>();
+
+        mainPanel.MainPanelInit();
+        mainPanel.startSceneButtons.Buttons[2].onClick.AddListener(OnOption);
+        optionPanel.OptionPanelInit();
+        optionPanel.buttons[2].onClick.AddListener(OnOption);
+
+        mainPanel.gameObject.SetActive(true);
+        optionPanel.gameObject.SetActive(false);
+    }
+
+    private void OnOption()
+    {
+        // 可记芒 难林扁
+        isOption = !isOption;
+
+        if (isOption) 
+        {
+            mainPanel.gameObject.SetActive(false);
+            optionPanel.gameObject.SetActive(true);
+        }
+        else
+        {
+            mainPanel.gameObject.SetActive(true);
+            optionPanel.gameObject.SetActive(false);
+        }
+    }
+}
